fc1690fc5b27341262215dbf246a80d53466d564
Drashti Jethva <61739589+jethvadrashti@users.noreply.github.com>
2022-02-19 22:41:37 +0530

    committed

---
+++ RecursionBacktracking/TargetSumSubset.java
@@ -0,0 +1,37 @@
+package DSALevel1.RecursionAndBacktracking.RecursionBacktracking;
+
+import java.util.Scanner;
+
+public class TargetSumSubset {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		int tar = sc.nextInt();
+		printTargetSumSubset(arr,0,"",0,tar);
+	}
+	public static void printTargetSumSubset(int[] arr,int idx,String set,int sos,int tar)
+	{
+		if(sos>tar)
+		{
+			return;
+		}
+		if(idx==arr.length)
+		{
+			if(sos==tar)
+			{
+				System.out.println(set+".");
+			}
+			return;
+		}
+		printTargetSumSubset(arr,idx+1,set+arr[idx]+",",sos+arr[idx],tar);
+		printTargetSumSubset(arr,idx+1,set,sos,tar);
+	}
+}
