f39aee1628617c90ad49f7b3f2e7a7399e24aa67
Drashti Jethva <61739589+jethvadrashti@users.noreply.github.com>
2022-03-08 10:22:31 +0530

    committed

--- StackAndQueue/DuplicateBrackets.java
+++ StackAndQueue/DuplicateBrackets.java
@@ -6,31 +6,32 @@ import java.util.Stack;
 
 public class DuplicateBrackets {
 	 public static  boolean isValid(String s) {
-	      Stack<Character> st = new Stack<>();
-	      for(int i=0;i<s.length();i++)
-	      {
-	    	  if(s.charAt(i)!= ')')
-	    	  {
-	    		  st.push(s.charAt(i));
-	    	  }
-	    	  else
-	    	  {
-	    		  if(st.peek()=='(')
-	    		  { 
-		    			  return true;
-	    		  }
-	    		  else
-	    		  {
-	    			  while(st.peek()!='(')
-	    			  {
-	    				  st.pop();
-	    			  }
-	    			  st.pop();
-	    		  }
-	    	  }
-	      }
-	      return false;
-	    }
+		 Stack<Character> st = new Stack<>();
+		 for(int i=0;i<s.length();i++)
+		 {
+			 char ch = s.charAt(i);
+			 if(ch!=')')
+			 {
+				 st.push(ch);
+			 }
+			 else
+			 {
+				 if(st.peek()=='(')
+				 {
+					 return true;
+				 }
+				 else
+				 {
+					 while(st.peek()!='(')
+					 {
+						 st.pop();
+					 }
+					 st.pop();
+				 }
+			 }
+		 }
+		 return false;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
