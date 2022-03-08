51b6d1c14419a625e9fdff01d79676b755ea6a9e
Drashti Jethva <61739589+jethvadrashti@users.noreply.github.com>
2022-03-08 11:20:38 +0530

    committed

--- StackAndQueue/BalancedBrackets.java
+++ StackAndQueue/BalancedBrackets.java
@@ -1,6 +1,6 @@
 package DSALevel1.StackAndQueue;
 //TC = O(n)
-//SC = O(1)
+//SC = O(n)
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -9,38 +9,65 @@ public class BalancedBrackets {
 	public static boolean isValid(String s)
 	{
 		Stack<Character> st = new Stack<>();
+		
 		for(int i=0;i<s.length();i++)
 		{
-			if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{')
+			char ch = s.charAt(i);
+			if(ch=='(' || ch=='{' || ch=='[')
 			{
-				st.push(s.charAt(i));
+				st.push(ch);
 			}
-			else if(s.charAt(i)==')' || s.charAt(i)==']' || s.charAt(i)=='}')
+			else if(ch==')')
 			{
-				if(st.size()==0)
+				boolean ans = closingBracketCheck(st, '(');
+				if(ans==false)
 				{
 					return false;
 				}
-				else
+			}
+			else if(ch=='}')
+			{
+				boolean ans = closingBracketCheck(st, '{');
+				if(ans==false)
 				{
-					if(st.peek()==s.charAt(i))
-					{
-						st.pop();
-					}
-					else
-					{
-						return false;
-					}
+					return false;
+				}
+			}
+			else if(ch==']')
+			{
+				boolean ans = closingBracketCheck(st, '[');
+				if(ans==false)
+				{
+					return false;
 				}
 			}
 		}
-		if(st.size()!=0)
+		if(st.size()==0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public static boolean closingBracketCheck(Stack<Character> st,char corch)
+	{
+		if(st.size()==0)
 		{
 			return false;
 		}
 		else
 		{
-			return true;
+			if(st.peek()==corch)
+			{
+				st.pop();
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
 	}
 	public static void main(String[] args) {
