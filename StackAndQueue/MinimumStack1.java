55b39bafd95a5ad964a8e3f69759b6a03edb2ab0
Drashti Jethva <61739589+jethvadrashti@users.noreply.github.com>
2022-03-19 10:16:12 +0530

    committed

---
+++ StackAndQueue/MinimumStack1.java
@@ -0,0 +1,82 @@
+package DSALevel1.StackAndQueue;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class MinimumStack1 {
+	 public static class MinStack {
+		    Stack<Integer> allData;
+		    Stack<Integer> minData;
+
+		    public MinStack() {
+		      allData = new Stack<>();
+		      minData = new Stack<>();
+		    }
+		    
+		    int size()
+		    {
+		    	return allData.size();
+		    }
+		    
+		    void push(int val)
+		    {
+		    	allData.push(val);
+		    	if(minData.size()==0 || val<=minData.peek())
+		    	{
+		    		minData.push(val);
+		    	}
+		    }
+		    int pop()
+		    {
+		    	int val = allData.pop();
+		    	if(val==minData.peek())
+		    	{
+		    		minData.pop();
+		    	}
+		    	return val;
+		    }
+		    int top()
+		    {
+		    	int val = allData.peek();
+		    	
+		    	return val;
+		    }
+		    int min()
+		    {
+		    	int val = minData.peek();
+		    	return val;
+		    }
+		    
+	 }
+		    public static void main(String[] args) throws Exception {
+		        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		        MinStack st = new MinStack();
+
+		        String str = br.readLine();
+		        while(str.equals("quit") == false){
+		          if(str.startsWith("push")){
+		            int val = Integer.parseInt(str.split(" ")[1]);
+		            st.push(val);
+		          } else if(str.startsWith("pop")){
+		            int val = st.pop();
+		            if(val != -1){
+		              System.out.println(val);
+		            }
+		          } else if(str.startsWith("top")){
+		            int val = st.top();
+		            if(val != -1){
+		              System.out.println(val);
+		            }
+		          } else if(str.startsWith("size")){
+		            System.out.println(st.size());
+		          } else if(str.startsWith("min")){
+		            int val = st.min();
+		            if(val != -1){
+		              System.out.println(val);
+		            }
+		          }
+		          str = br.readLine();
+		        }
+		    }
+}
