5135ca6e859b89a3fb1e5d928f9115a56f3bd42d
Drashti Jethva <61739589+jethvadrashti@users.noreply.github.com>
2022-05-27 14:14:13 +0530

    committed

---
+++ BinaryTree/TiltOfBT.java
@@ -0,0 +1,118 @@
+package BinaryTree;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+import BinaryTree.BTBasics.Node;
+import BinaryTree.BTBasics.Pair;
+
+public class TiltOfBT {
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data,Node left,Node right)
+		{
+			this.data = data;
+			this.left = left;
+			this.right= right;
+		}
+	}
+	public static class Pair{
+		Node node;
+		int state;
+		
+		Pair(Node node,int state)
+		{
+			this.node = node;
+			this.state = state;
+		}
+	}
+	public static Node construct(Integer[] arr)
+	{
+		Node root = new Node(arr[0],null,null);
+		
+		Stack<Pair> st = new Stack<>();
+		
+		st.push(new Pair(root,1));
+		int idx = 0;
+		while(st.size()>0)
+		{
+			Pair top = st.peek();
+			
+			if(top.state==1)
+			{
+				idx++;
+				if(arr[idx] !=null)
+				{
+					Node lc = new Node(arr[idx],null,null);
+					top.node.left = lc;
+					st.push(new Pair(top.node.left,1));
+				}
+				else
+				{
+					top.node.left = null;
+				}
+				top.state++;
+			}
+			else if(top.state==2)
+			{
+				idx++;
+				if(arr[idx] !=null)
+				{
+					Node rc = new Node(arr[idx],null,null);
+					top.node.right = rc;
+					st.push(new Pair(top.node.right,1));
+				}
+				else
+				{
+					top.node.right = null;
+				}
+				top.state++;
+			}
+			else
+			{
+				st.pop();
+			}
+		}
+		
+		return root;
+	}
+	static int tilt = 0;
+	public static int tiltOfBT(Node node)
+	{
+		if(node==null)
+		{
+			return 0;
+		}
+		
+		int ls = tiltOfBT(node.left);
+		int rs = tiltOfBT(node.right);
+		
+		tilt += Math.abs(ls-rs);
+		int ts = ls + rs + node.data;
+		return ts;
+	}
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int n = Integer.parseInt(br.readLine());
+	    Integer[] arr = new Integer[n];
+	    String[] values = br.readLine().split(" ");
+	    for (int i = 0; i < n; i++) {
+	      if (values[i].equals("n") == false) {
+	        arr[i] = Integer.parseInt(values[i]);
+	      } else {
+	        arr[i] = null;
+	      }
+	    }
+
+	    Node root = construct(arr);
+	    
+	    tiltOfBT(root);
+	    
+	    System.out.println(tilt);
+	}
+}
